466e0d932993df2da7cd74ea2bf4c094f17097f8
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-07-07 16:35:31 +0900

    add rxjava2 sample

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,6 +2,48 @@ package app;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    Flowable<String> flowable = Flowable.create(new FlowableONSubscribe<String>() {
+      @Override
+      public void subscribe(FlowableEmitter<String> emitter) throws Exception {
+        String[] data = {"data1", "data2"};
+        for(String item : data) {
+          if (emitter.isCancelled()) {
+            return;
+          }
+          emitter.onNext(item);
+        }
+        emitter.onComplete(); 
+      }
+    }, BackpressureStrategy.BUFFER);
+
+    flowable.observeOn(Schedulers.computation())
+            .subscribe(new Subscriber<String>() {
+              private Subscription subscription;
+
+              @Override
+              public void onSubscribe(Subscription subscription) {
+                this.subscription = subscription;
+                this.subscription.request(1L);
+              }
+
+              @Override
+              public void onNext(String data) {
+                String threadName = Thread.currentThread().getName();
+                System.out.println(threadName + " : " + data);
+                this.subscription.request(1L);
+              }
+
+              @Override
+              public void onComplete() {
+                String threadName = Thread.currentThread().getName();
+                System.out.println(htreadName + " : DONE");
+              }
+              
+              @Override
+              public void onError(Throwable error) {
+                error.printStackTrace();
+              }
+          });
+    Thread.sleep(500L);
   }
 }
